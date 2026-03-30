50c758d32d9be1f36a6d884c95c5117d19e841c3
Gwen <gwen.thelin@proton.me>
2026-03-29 21:07:01 -0600

    Some quality of life changes
    Made it so that an immedite exti meets you with a goodbye. Also tells
    user that they type exit to exit.

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,44 @@ use std::io;
 use std::thread;
 use std::time::Duration;
 
+struct SessionData {
+    is_empty: bool,
+}
+
+impl SessionData {
+    fn swap_emptiness(&mut self) {
+        self.is_empty = !self.is_empty;
+    }
+}
+
 fn main() {
+    let mut session = SessionData { is_empty: false };
+
     println!("Welcome to the decider!");
     //A sleep call to make the program feel more dynamic and human
     thread::sleep(Duration::from_secs(1));
     //TODO: Tell user to type "exit" to exit
     let mut iteration: u8 = 1;
-    for option in input_loop().iter() {
-        print!("{iteration}. ");
-        print!("{option}");
-        iteration += 1;
-        println!();
+
+    for option in input_loop(&mut session).iter() {
+        if !session.is_empty {
+            print!("{iteration}. ");
+            print!("{option}");
+            iteration += 1;
+            println!();
+        }
+    }
+
+    if session.is_empty {
+        print!("Goodbye!");
     }
 }
 
 //I'm using a set so I don't have to import random & because it's quick
-fn input_loop() -> HashSet<String> {
+fn input_loop(session: &mut SessionData) -> HashSet<String> {
     let mut options = HashSet::new();
     loop {
+        println!("type \"exit\" to exit");
         println!("Write an option:");
         let mut input = String::new();
         io::stdin()
@@ -31,6 +51,13 @@ fn input_loop() -> HashSet<String> {
         if trimmed_input.contains("exit") {
             if options.is_empty() {
                 options.insert("".to_string());
+                if !session.is_empty {
+                    session.swap_emptiness();
+                }
+            } else {
+                if session.is_empty {
+                    session.swap_emptiness();
+                }
             }
             println!();
             thread::sleep(Duration::from_secs(2));
